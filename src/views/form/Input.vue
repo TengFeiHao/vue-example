3922417fb4cc15cf9cd3f14ab1124668d8d7de30
TengFeiHao <374521128@qq.com>
2020-12-23 11:45:16 +0800

    fix:valide

--- src/views/form/Input.vue
+++ src/views/form/Input.vue
@@ -40,7 +40,7 @@ export default {
       this.$emit('input', e.target.value)
       console.log(this.$parent)
       // 通知父级执行校验 父级此时还是个slot 如何派发事件 $emit不适合，$parent $root 可以实现，但是利用父元素或者根元素显然有耦合的问题
-      // this.$parent.$emit('validate')
+      // this.$parent.$emit('validate');
       this.dispatch('formItem', 'validate', [e.target.value]);
     }
   }
