f51fb9e0658cbb23b26c12d8ba247375ade311ab
TengFeiHao <374521128@qq.com>
2020-12-24 12:05:53 +0800

    fix:render和extend封装Notice

--- src/assets/utils/createExtend.js
+++ src/assets/utils/createExtend.js
@@ -3,10 +3,9 @@ import Notice from '../../components/Notice.vue'
 // extend 使用基础 Vue 构造器，创建一个“子类”。参数是一个包含组件选项的对象。
 const constructor = Vue.extend(Notice)
 
-let notice;
 const createExtend = (options) => {
   // propsData 创建实例时传递 props
-  notice = new constructor({
+  let notice = new constructor({
     propsData: {
       ...options
     }
@@ -18,7 +17,6 @@ const createExtend = (options) => {
   }
   document.body.appendChild(notice.$el);
   notice.show()
-  return notice;
 }
 
 export default createExtend;
